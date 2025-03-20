d67676441ab6f9420fb3f994a95d769b07d4620b
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-03-20 18:31:34 +0300

    Add meal logic.

--- src/main/java/com/example/foodcalc/controller/UserController.java
+++ src/main/java/com/example/foodcalc/controller/UserController.java
@@ -1,18 +1,12 @@
 package com.example.foodcalc.controller;
 
-import com.example.foodcalc.dto.DishDto;
-import com.example.foodcalc.dto.DishResponseDto;
-import com.example.foodcalc.dto.UserDto;
-import com.example.foodcalc.dto.UserResponseDto;
+import com.example.foodcalc.dto.*;
 import com.example.foodcalc.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/foodcalc")
@@ -36,10 +30,10 @@ public class UserController {
         return new ResponseEntity<>(dishResponseDto, HttpStatus.OK);
     }
 
-    @PostMapping("/meal")
-    public ResponseEntity<UserResponseDto> addMeal(@Valid @RequestBody UserDto dto){
-        UserResponseDto userResponseDto = userService.addUser(dto);
-        return new ResponseEntity<>(userResponseDto, HttpStatus.OK);
+    @PostMapping("/user/{userId}/meal")
+    public ResponseEntity<MealResponseDto> addMeal(@Valid @RequestBody MealDto dto, @PathVariable long userId){
+        MealResponseDto mealResponseDto = userService.addMeal(dto, userId);
+        return new ResponseEntity<>(mealResponseDto, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/foodcalc/service/UserService.java
+++ src/main/java/com/example/foodcalc/service/UserService.java
@@ -1,19 +1,24 @@
 package com.example.foodcalc.service;
 
-import com.example.foodcalc.dto.DishDto;
-import com.example.foodcalc.dto.DishResponseDto;
-import com.example.foodcalc.dto.UserDto;
-import com.example.foodcalc.dto.UserResponseDto;
+import com.example.foodcalc.dto.*;
 import com.example.foodcalc.entity.Dish;
+import com.example.foodcalc.entity.Meal;
 import com.example.foodcalc.entity.User;
+import com.example.foodcalc.exception.UserException;
 import com.example.foodcalc.mapper.DishMapper;
 import com.example.foodcalc.mapper.UserMapper;
 import com.example.foodcalc.repository.DishRepository;
+import com.example.foodcalc.repository.MealRepository;
 import com.example.foodcalc.repository.UserRepository;
 import com.example.foodcalc.type.Gender;
 import com.example.foodcalc.type.WeightStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,14 +26,17 @@ public class UserService {
     private final UserMapper userMapper;
     private final DishRepository dishRepository;
     private final DishMapper dishMapper;
+    private final MealRepository mealRepository;
 
     @Autowired
     public UserService(UserRepository userRepository, UserMapper userMapper,
-                       DishRepository dishRepository, DishMapper dishMapper) {
+                       DishRepository dishRepository, DishMapper dishMapper,
+                       MealRepository mealRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.dishRepository = dishRepository;
         this.dishMapper = dishMapper;
+        this.mealRepository = mealRepository;
     }
 
     public UserResponseDto addUser(UserDto dto){
@@ -49,6 +57,33 @@ public class UserService {
         dish = dishRepository.save(dish);
         return DishResponseDto.builder()
                 .dishId(dish.getId()).build();
+    }
+
+    public MealResponseDto addMeal(MealDto dto, Long userId){
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isEmpty())
+            throw new UserException("Пользователь с данным id не существует.");
+        User user = optionalUser.get();
+
+        List<Dish> dishes = new ArrayList<>();
+        int calories = 0;
+        for (long dishId: dto.getDishes()){
+            Optional<Dish> optionalDish = dishRepository.findById(dishId);
+            if (optionalDish.isEmpty())
+                throw new UserException(String.format("Ресурс с данным id %d не существует.", dishId));
+            calories += optionalDish.get().getCaloriesPerServing();
+            dishes.add(optionalDish.get());
+        }
+
+        Meal meal = Meal.builder()
+                .mealDate(LocalDate.now())
+                .dishes(dishes)
+                .calories(calories)
+                .user(user).build();
+
+        meal = mealRepository.save(meal);
+        return MealResponseDto.builder()
+                .mealId(meal.getId()).build();
     }
 
     private int calcDailyNorm(Short height, Short weight, Short age,
