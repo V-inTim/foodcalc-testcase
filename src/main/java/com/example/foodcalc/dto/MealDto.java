6888f797caa2a89f2a47c21098c00c8a4710f644
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-03-20 18:30:26 +0300

    Add dto for adding meal.

---
+++ src/main/java/com/example/foodcalc/dto/MealDto.java
@@ -0,0 +1,16 @@
+package com.example.foodcalc.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MealDto {
+    @NotNull
+    List<Long> dishes;
+}
